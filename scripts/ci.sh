b9257e0e706b2b20ae48d460de4778a6f7dd9402
Derek Bekoe <debekoe@microsoft.com>
2017-05-24 15:56:53 -0700

    Add pylint and flake8 and ci script

---
+++ scripts/ci.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+set -e
+
+scripts_root=$(cd $(dirname $0); pwd)
+
+python -m pylint clicore --rcfile=.pylintrc -r n -d I0013
+
+flake8 --statistics --append-config=.flake8
+
+python $scripts_root/license_verify.py
+
+
