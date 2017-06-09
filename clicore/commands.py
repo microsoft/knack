817178bb5624dd2baef24c2a45f66b72a05c5f24
Derek Bekoe <debekoe@microsoft.com>
2017-06-08 17:51:39 -0700

    Support the cli_command & CommandGroup formats of specifying commands (#7)

--- clicore/commands.py
+++ clicore/commands.py
@@ -3,18 +3,27 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+import types
+import copy
 from collections import OrderedDict
+from importlib import import_module
 
+import six
+
+from .prompting import prompt_y_n, NoTTYException
+from .util import CLIError
 from .log import get_logger
 
 logger = get_logger(__name__)
 
 
-class CliCommand(object):  # pylint:disable=too-many-instance-attributes
+class CLICommand(object):  # pylint:disable=too-many-instance-attributes
 
-    def __init__(self, name, handler, description=None, table_transformer=None,
+    # pylint: disable=unused-argument
+    def __init__(self, ctx, name, handler, description=None, table_transformer=None,
                  arguments_loader=None, description_loader=None,
-                 formatter_class=None, deprecate_info=None):
+                 formatter_class=None, deprecate_info=None, **kwargs):
+        self.ctx = ctx
         self.name = name
         self.handler = handler
         self.help = None
@@ -25,14 +34,20 @@ class CliCommand(object):  # pylint:disable=too-many-instance-attributes
         self.formatter_class = formatter_class
         self.deprecate_info = deprecate_info
 
-    def should_load_description(self):  # pylint: disable=no-self-use
-        # TODO self.ctx should be passed in somehow...
-        # return not self.ctx.data['completer_active']
-        return True
+    def should_load_description(self):
+        return not self.ctx.data['completer_active']
 
     def load_arguments(self):
         if self.arguments_loader:
             self.arguments.update(self.arguments_loader())
+
+    def add_argument(self, param_name, *option_strings, **kwargs):
+        pass
+        # dest = kwargs.pop('dest', None)
+        # TODO Params
+        # argument = CliCommandArgument(
+        #     dest or param_name, options_list=option_strings, **kwargs)
+        # self.arguments[param_name] = argument
 
     def execute(self, **kwargs):
         return self(**kwargs)
@@ -48,6 +63,9 @@ class CliCommand(object):  # pylint:disable=too-many-instance-attributes
 
 class CLICommandsLoader(object):
 
+    # TODO Param Confirmation & No-wait are extensions that should be added in an extensible way.
+    CONFIRM_PARAM_NAME = 'yes'
+
     def __init__(self, ctx=None):
         self.ctx = ctx
         # A command table is a dictionary of name -> CliCommand instances
@@ -56,3 +74,119 @@ class CLICommandsLoader(object):
     def generate_command_table(self, args):  # pylint: disable=unused-argument
         self.ctx.raise_event('Commands.OnGenerateCommandTable', cmd_tbl=self.command_table)
         return OrderedDict(self.command_table)
+
+    def cli_command(self, module_name, name, operation, **kwargs):
+        """ Add a command to the command table. """
+        self.command_table[name] = self.create_command(module_name, name, operation, **kwargs)
+
+    def create_command(self, module_name, name, operation, **kwargs):  # pylint: disable=unused-argument
+        if not isinstance(operation, six.string_types):
+            raise ValueError("Operation must be a string. Got '{}'".format(operation))
+
+        name = ' '.join(name.split())
+
+        confirmation = kwargs.get('confirmation', False)
+        client_factory = kwargs.get('client_factory', None)
+
+        def _command_handler(command_args):
+            # TODO Event for pre command handler execution
+            if confirmation \
+                and not command_args.get(CLICommandsLoader.CONFIRM_PARAM_NAME) \
+                and not self.ctx.config.getboolean('core', 'disable_confirm_prompt', fallback=False) \
+                    and not CLICommandsLoader.user_confirmed(confirmation, command_args):
+                # TODO Event for cancelled operation.
+                raise CLIError('Operation cancelled.')
+            op = CLICommandsLoader.get_op_handler(operation)
+            client = client_factory(command_args) if client_factory else None
+            result = op(client, **command_args) if client else op(**command_args)
+            return result
+
+        def arguments_loader():
+            # TODO Params
+            pass
+            # return extract_args_from_signature(get_op_handler(operation), no_wait_param=no_wait_param)
+
+        def description_loader():
+            # TODO Params
+            pass
+            # return extract_full_summary_from_signature(get_op_handler(operation))
+
+        kwargs['arguments_loader'] = arguments_loader
+        kwargs['description_loader'] = description_loader
+
+        cmd = CLICommand(self.ctx, name, _command_handler, **kwargs)
+        # TODO Event to add any other arguments so that this is extensible.
+        if confirmation:
+            cmd.add_argument(CLICommandsLoader.CONFIRM_PARAM_NAME, '--yes', '-y',
+                             action='store_true',
+                             help='Do not prompt for confirmation')
+        return cmd
+
+    @staticmethod
+    def get_op_handler(operation):
+        """ Import and load the operation handler """
+        try:
+            mod_to_import, attr_path = operation.split('#')
+            op = import_module(mod_to_import)
+            for part in attr_path.split('.'):
+                op = getattr(op, part)
+            if isinstance(op, types.FunctionType):
+                return op
+            return six.get_method_function(op)
+        except (ValueError, AttributeError):
+            raise ValueError("The operation '{}' is invalid.".format(operation))
+
+    @staticmethod
+    def user_confirmed(confirmation, command_args):
+        if callable(confirmation):
+            return confirmation(command_args)
+        try:
+            if isinstance(confirmation, six.string_types):
+                return prompt_y_n(confirmation)
+            return prompt_y_n('Are you sure you want to perform this operation?')
+        except NoTTYException:
+            logger.warning('Unable to prompt for confirmation as no tty available. Use --yes.')
+            return False
+
+
+class CommandSuperGroup(object):
+    def __init__(self, module_name, command_loader, operations_tmpl, **kwargs):
+        self.module_name = module_name
+        self.command_loader = command_loader
+        self.operations_tmpl = operations_tmpl
+        self.super_group_kwargs = kwargs
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def group(self, group_name, **kwargs):
+        group_args = copy.deepcopy(self.super_group_kwargs)
+        group_args.update(kwargs)
+        return CommandGroup(self.module_name, self.command_loader, group_name, self.operations_tmpl, **group_args)
+
+
+class CommandGroup(object):
+    def __init__(self, module_name, command_loader, group_name, operations_tmpl, **kwargs):
+        self.module_name = module_name
+        self.command_loader = command_loader
+        self.group_name = group_name
+        self.operations_tmpl = operations_tmpl
+        self.group_kwargs = kwargs
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def command(self, name, method_name, **kwargs):
+        command_name = '{} {}'.format(self.group_name, name)
+        command_kwargs = copy.deepcopy(self.group_kwargs)
+        command_kwargs.update(kwargs)
+        self.command_loader.cli_command(self.module_name,
+                                        command_name,
+                                        self.operations_tmpl.format(method_name),
+                                        **command_kwargs)
