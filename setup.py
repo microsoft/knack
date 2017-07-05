a190b5cc480183f9ac41ea3fbdfb6bee71af40d9
Derek Bekoe <debekoe@microsoft.com>
2017-07-05 14:31:31 -0700

    Release 0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from __future__ import print_function
 from codecs import open
 from setuptools import setup, find_packages
 
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 
 DEPENDENCIES = [
     'argcomplete',
