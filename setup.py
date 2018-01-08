fb412905b45b828525f8450b7b23975c356e36fb
Derek Bekoe <debekoe@microsoft.com>
2018-01-08 09:39:31 -0800

    Change version to 0.3.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from __future__ import print_function
 from codecs import open
 from setuptools import setup, find_packages
 
-VERSION = '0.3.0'
+VERSION = '0.3.1'
 
 DEPENDENCIES = [
     'argcomplete',
