317b2511fbdec64cf20653221b93837daa3e2296
yugangw-msft <yugangw@microsoft.com>
2018-04-24 17:25:39 -0700

    use filter

--- knack/util.py
+++ knack/util.py
@@ -54,12 +54,14 @@ def to_snake_case(s):
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
 
-def todict(obj):  # pylint: disable=too-many-return-statements
-
+def todict(obj, value_filter=None):  # pylint: disable=too-many-return-statements
+    """ 
+    Convert an object to a dictionary. Use 'value_filter' to ignore specified values
+    """
     if isinstance(obj, dict):
-        return {k: todict(v) for (k, v) in obj.items()}
+        return {k: todict(v, value_filter) for (k, v) in obj.items() if (not value_filter or value_filter(obj, k, v))}
     elif isinstance(obj, list):
-        return [todict(a) for a in obj]
+        return [todict(a, value_filter) for a in obj]
     elif isinstance(obj, Enum):
         return obj.value
     elif isinstance(obj, (date, time, datetime)):
@@ -67,9 +69,9 @@ def todict(obj):  # pylint: disable=too-many-return-statements
     elif isinstance(obj, timedelta):
         return str(obj)
     elif hasattr(obj, '_asdict'):
-        return todict(obj._asdict())
+        return todict(obj._asdict(), value_filter)
     elif hasattr(obj, '__dict__'):
-        return dict([(to_camel_case(k), todict(v))
+        return dict([(to_camel_case(k), todict(v, value_filter))
                      for k, v in obj.__dict__.items()
-                     if not callable(v) and not k.startswith('_')])
+                     if not callable(v) and not k.startswith('_') and (not value_filter or value_filter(obj, k, v))])
     return obj
