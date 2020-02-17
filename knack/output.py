2e7f6852b5d370383b4157a1c2a82e036311c55a
Jiashuo Li <jiasli@microsoft.com>
2020-02-17 21:02:14 +0800

    refactor

--- knack/output.py
+++ knack/output.py
@@ -7,6 +7,7 @@ from __future__ import print_function
 
 import errno
 import json
+import yaml
 import traceback
 import sys
 from collections import OrderedDict
@@ -47,14 +48,11 @@ def format_json_color(obj):
 
 
 def format_yaml(obj):
-    from yaml import (safe_dump, representer)
-    import json
-
     try:
-        return safe_dump(obj.result, default_flow_style=False, allow_unicode=True)
-    except representer.RepresenterError:
+        return yaml.safe_dump(obj.result, default_flow_style=False, allow_unicode=True)
+    except yaml.representer.RepresenterError:
         # yaml.safe_dump fails when obj.result is an OrderedDict. knack's --query implementation converts the result to an OrderedDict. https://github.com/microsoft/knack/blob/af674bfea793ff42ae31a381a21478bae4b71d7f/knack/query.py#L46. # pylint: disable=line-too-long
-        return safe_dump(json.loads(json.dumps(obj.result)), default_flow_style=False, allow_unicode=True)
+        return yaml.safe_dump(json.loads(json.dumps(obj.result)), default_flow_style=False, allow_unicode=True)
 
 
 def format_yaml_color(obj):
